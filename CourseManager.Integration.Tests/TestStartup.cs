b22b4b43eb23e5b3cb082a814dff81ddd12b982d
Sergio Perez <sergio.ontiveros.perez@outlook.com>
2023-02-02 07:10:32 -0300

    initial commit

---
+++ CourseManager.Integration.Tests/TestStartup.cs
@@ -0,0 +1,33 @@
+﻿using CourseManager.DataBase.SqlServer;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace CourseManager.Integration.Tests
+{
+  public class TestStartup : Startup
+  {
+    public new IConfiguration Configuration { get; }
+
+    public TestStartup(IConfiguration configuration) : base(configuration)
+    {
+      Configuration = configuration;
+    }
+    protected override void ConfigureDb(IServiceCollection services)
+    {
+      var controllersAssemblyType = typeof(Startup).GetTypeInfo().Assembly;
+      services.AddMvc().AddApplicationPart(controllersAssemblyType);
+
+      var serviceProvider = services.BuildServiceProvider();
+      var options = serviceProvider.GetService<DbContextOptions<CourseManagerDbContext>>();
+      services.AddScoped(s => new FakeDbContext(options));
+    }
+
+    protected override void ConfigSpa(IApplicationBuilder app, IWebHostEnvironment env) {}
+    protected override void AddSpaServices(IServiceCollection services){}
+    protected override void MigrateDB(IApplicationBuilder app){}
+  }
+}
